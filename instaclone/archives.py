b62e3bf374f8715272ad70850d0136e0cf301a28
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 15:40:56 +0000

    Adding tarfile member sanitization to extractall()

--- instaclone/archives.py
+++ instaclone/archives.py
@@ -88,7 +88,26 @@ def targz_dir(source_dir, target_archive, dereference_ext_symlinks=True):
 
 def untargz_dir(source_archive, target_dir):
   with tarfile.open(source_archive, "r:gz") as tf:
-    tf.extractall(path=target_dir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tf, path=target_dir)
 
 
 TarGzArchiver = _Archiver(".tar.gz", targz_dir, untargz_dir)
